80706d1d25ab51e925947be88470f542f0cce82e
Giddygangster85 <92816670+Giddygangster85@users.noreply.github.com>
2021-12-01 14:17:09 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,27 @@
+function Alarm () {
+    music.setVolume(0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    music.playTone(494, music.beat(BeatFraction.Double))
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    music.playTone(262, music.beat(BeatFraction.Double))
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    music.playTone(494, music.beat(BeatFraction.Double))
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    music.playTone(262, music.beat(BeatFraction.Double))
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    music.playTone(494, music.beat(BeatFraction.Double))
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    music.playTone(262, music.beat(BeatFraction.Double))
+}
 let Pressure_value = 0
 radio.setGroup(130)
 basic.forever(function () {
     Pressure_value = pins.analogReadPin(AnalogPin.P0)
+    basic.showNumber(pins.analogReadPin(AnalogPin.P0))
     if (Pressure_value < 900) {
         radio.sendNumber(0)
+        Alarm()
+    } else if (Pressure_value > 900) {
+        radio.sendNumber(1)
     }
 })
