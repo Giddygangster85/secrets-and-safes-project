c1116e1e7a5cb2d53782795c81fdd908f68646d3
Giddygangster85 <92816670+Giddygangster85@users.noreply.github.com>
2021-12-02 14:11:13 -0500

    Update2: On/Off has begun and Reaction is almost complete, need to code reactory and reciprocal statements for receiver

--- main.ts
+++ main.ts
@@ -1,27 +1,39 @@
-function Alarm () {
-    music.setVolume(0)
-    pins.digitalWritePin(DigitalPin.P2, 1)
-    music.playTone(494, music.beat(BeatFraction.Double))
-    pins.digitalWritePin(DigitalPin.P2, 0)
-    music.playTone(262, music.beat(BeatFraction.Double))
-    pins.digitalWritePin(DigitalPin.P2, 1)
-    music.playTone(494, music.beat(BeatFraction.Double))
-    pins.digitalWritePin(DigitalPin.P2, 0)
-    music.playTone(262, music.beat(BeatFraction.Double))
-    pins.digitalWritePin(DigitalPin.P2, 1)
-    music.playTone(494, music.beat(BeatFraction.Double))
-    pins.digitalWritePin(DigitalPin.P2, 0)
-    music.playTone(262, music.beat(BeatFraction.Double))
-}
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        OnOff = receivedNumber
+    } else if (receivedNumber == 1) {
+        OnOff = receivedNumber
+    } else if (receivedNumber == 2) {
+        Reaction = 0
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    OnOff = 0
+})
+input.onButtonPressed(Button.AB, function () {
+    Reaction = 2
+})
+input.onButtonPressed(Button.B, function () {
+    OnOff = 1
+})
 let Pressure_value = 0
+let Reaction = 0
+let OnOff = 0
 radio.setGroup(130)
+OnOff = 0
 basic.forever(function () {
     Pressure_value = pins.analogReadPin(AnalogPin.P0)
-    basic.showNumber(pins.analogReadPin(AnalogPin.P0))
-    if (Pressure_value < 900) {
-        radio.sendNumber(0)
-        Alarm()
-    } else if (Pressure_value > 900) {
-        radio.sendNumber(1)
+    basic.showNumber(Pressure_value)
+    if (OnOff == 0) {
+        basic.showString("Alarm Deactive")
+    } else if (OnOff == 1) {
+        basic.showString("Alarm Active")
+        if (Pressure_value < 900) {
+            radio.sendNumber(0)
+        } else if (Pressure_value > 900) {
+            radio.sendNumber(1)
+        }
+    } else if (Reaction == 2) {
+        basic.showString("Cops Have Been Called")
     }
 })
