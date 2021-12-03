c4210f1d51bbc00c4c6d90cfadcb06b3fba3f839
Giddygangster85 <92816670+Giddygangster85@users.noreply.github.com>
2021-12-03 14:16:54 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,36 +4,36 @@ radio.onReceivedNumber(function (receivedNumber) {
     } else if (receivedNumber == 1) {
         OnOff = receivedNumber
     } else if (receivedNumber == 2) {
-        Reaction = 0
+        OnOff = receivedNumber
     }
 })
 input.onButtonPressed(Button.A, function () {
     OnOff = 0
 })
 input.onButtonPressed(Button.AB, function () {
-    Reaction = 2
+    OnOff = 2
 })
 input.onButtonPressed(Button.B, function () {
     OnOff = 1
 })
 let Pressure_value = 0
-let Reaction = 0
 let OnOff = 0
 radio.setGroup(130)
-OnOff = 0
+OnOff = 1
 basic.forever(function () {
-    Pressure_value = pins.analogReadPin(AnalogPin.P0)
-    basic.showNumber(Pressure_value)
     if (OnOff == 0) {
-        basic.showString("Alarm Deactive")
+        basic.showIcon(IconNames.No)
     } else if (OnOff == 1) {
-        basic.showString("Alarm Active")
+        Pressure_value = pins.analogReadPin(AnalogPin.P0)
+        basic.showNumber(Pressure_value)
         if (Pressure_value < 900) {
             radio.sendNumber(0)
+            basic.showIcon(IconNames.Sad)
         } else if (Pressure_value > 900) {
             radio.sendNumber(1)
+            basic.showIcon(IconNames.Happy)
         }
-    } else if (Reaction == 2) {
+    } else if (OnOff == 2) {
         basic.showString("Cops Have Been Called")
     }
 })
