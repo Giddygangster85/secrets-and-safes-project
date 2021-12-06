ca49ff582e645b38fd4cbf091398151965ea9651
Giddygangster85 <92816670+Giddygangster85@users.noreply.github.com>
2021-12-06 14:12:16 -0500

    Comments have been set and project is presentable and in final stages.

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 radio.onReceivedNumber(function (receivedNumber) {
+    // asks if the received signal is meant to turn the alarm on/off or to call the police.
     if (receivedNumber == 0) {
         OnOff = receivedNumber
     } else if (receivedNumber == 1) {
@@ -7,25 +8,32 @@ radio.onReceivedNumber(function (receivedNumber) {
         OnOff = receivedNumber
     }
 })
+// Manually turns the alarm off
 input.onButtonPressed(Button.A, function () {
     OnOff = 0
 })
+// Manually contacts local authorities.
 input.onButtonPressed(Button.AB, function () {
     OnOff = 2
 })
+// Manually turns the alarm on
 input.onButtonPressed(Button.B, function () {
     OnOff = 1
 })
+// Establishes radio group and also sets the alarm as disengaged.
 let Pressure_value = 0
 let OnOff = 0
 radio.setGroup(130)
-OnOff = 1
+OnOff = 0
 basic.forever(function () {
+    // This asks if the user had the Alarm engaged and whether or not they have called the cops.
     if (OnOff == 0) {
         basic.showIcon(IconNames.No)
     } else if (OnOff == 1) {
+        // detects the value on the pressure sensor which rises when an object is on top of it and decreases when no object is present.
         Pressure_value = pins.analogReadPin(AnalogPin.P0)
         basic.showNumber(Pressure_value)
+        // This code asks whether or not the pressure sensor is detecting enough pressure for the item to still be there and if not then a signal is sent to set of an alarm.
         if (Pressure_value < 900) {
             radio.sendNumber(0)
             basic.showIcon(IconNames.Sad)
@@ -34,6 +42,7 @@ basic.forever(function () {
             basic.showIcon(IconNames.Happy)
         }
     } else if (OnOff == 2) {
+        // Warns the robber to return the objects as the cops have been called.
         basic.showString("Cops Have Been Called")
     }
 })
